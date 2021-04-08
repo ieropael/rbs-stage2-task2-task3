769d342110064db073e04b032ed9e4ecb4926e72
Aleksandr Petrov <ieropael@gmail.com>
2021-04-08 14:20:33 +0400

    move event listeners from ui configuration and add comments

--- script.js
+++ script.js
@@ -9,25 +9,24 @@ webix.ready(function () {
 
   var basket = [];
 
-  var summary = [{ id: "sum", title: "Сумма", value: 0 }];
+  var summary = [ { id: "sum", title: "Сумма", value: 0 } ];
 
+  // Отображение таблиц "Корзина-Склад"
   webix.ui({
     rows: [
       {
         cols: [
-          // Show modal window
+          // Тулбар с кнопкой "Новый товар", при нажатии на которую открывается
+          // окно с формой добавления нового товара
           {
             view:"toolbar",
             id:"top_toolbar",
             elements: [
               { },
-              { view:"button", id:"new_product_button", value:"Новый товар", width: 200,
-              click:function(){
-                $$("add_window").show();
-              }}
+              { view:"button", id:"new_product_button", value:"Новый товар", width: 200 }
             ]
           },
-          // Summary
+          // Показ суммы стоимости товаров, находящихся в корзине
           {
             view: "list",
             id: "summary",
@@ -38,7 +37,7 @@ webix.ready(function () {
       },
       {
         cols: [
-          // Stock datatable
+          // Таблица с данными "Склад"
           {
             view: "datatable",
             id: "stock",
@@ -49,14 +48,9 @@ webix.ready(function () {
               { id: "title", header: "Название", fillspace: true },
               { id: "quantity", header: "Количество", width: 100 },
               { id: "cost", header: "Цена", width: 100 },
-            ],
-            on: {
-              onItemClick: function () {
-                transfer($$("stock"), $$("basket"), $$("stock").getSelectedItem());
-              }
-            }
+            ]
           },
-          // Basket datatable
+          // Таблица с данными "Корзина"
           {
             view: "datatable",
             id: "basket",
@@ -67,20 +61,14 @@ webix.ready(function () {
                 { id: "title", header: "Название", fillspace: true },
                 { id: "quantity", header: "Количество", width: 100 },
                 { id: "cost", header: "Цена", width: 100 },
-            ],
-            on: {
-              onItemClick: function () {
-                transfer($$("basket"), $$("stock"), $$("basket").getSelectedId());
-              }
-            }
-          },
-        ],
-      },
+            ]
+          }
+        ]
+      }
     ]
   });
 
-  // Modal window
-
+  // Модальное окно с формой добавления нового товара
   webix.ui({
     view:"window",
     id: "add_window",
@@ -92,18 +80,16 @@ webix.ready(function () {
         {
           template: "Добавление нового товара"
         },
+        // Кнопка закрытия формы добавления нового товара,
+        // при нажатии на которую очищаются поля формы
         {
-          view:"icon", icon:"wxi-close", click: function() {
-            $$("add_form").clear();
-            $$("add_form").clearValidation();
-            $$("add_window").hide();
-          }
+          view:"icon", id: "close_form_button", icon:"wxi-close"
         }
       ]    
     },
     body: {
       rows: [
-        // Form
+        // Форма добавления нового товара
         { 
           view:"form",
           id:"add_form",
@@ -117,30 +103,55 @@ webix.ready(function () {
             { view:"text", name:"cost", id:"input_cost", label:"Цена",
                 invalidMessage: "Введите стоимость товара (число больше ноля)" }
           ],
+          // Правила валидации данных в форме
           rules:{
             title:webix.rules.isNotEmpty,
             quantity:function(value) { return parseFloat(value) == value && value > 0; },
             cost:function(value) { return parseFloat(value) == value && value >= 0; }
           },
         },
-        // Add button
+        // Кнопка добавления данных нового товара из формы в таблицу "Склад",
+        // но если данные не прошли валидацию, появятся сообщения об ошибках
         {
-          view:"button", id:"add_product", value:"Добавить",
-          click: function() {
-            if ($$("add_form").validate()) {
-              addToStock($$("add_form").getValues());
-              refresh();
-              $$("add_form").clear();
-              $$("add_window").hide();
-            }
-          }
+          view:"button", id:"add_product", value:"Добавить"
         },
       ]
     }
   });
 
-  // Functions
+  // Обработка нажатия на строку товара в таблице "Склад"
+  $$("stock").attachEvent("onItemClick", function () {
+    transfer($$("stock"), $$("basket"), $$("stock").getSelectedItem());
+  });
 
+  // Обработка нажатия на строку товара в таблице "Корзина"
+  $$("basket").attachEvent("onItemClick", function () {
+    transfer($$("basket"), $$("stock"), $$("basket").getSelectedId());
+  });
+  
+  // Обработка нажатия на кнопку "Новый товар"
+  $$("new_product_button").attachEvent("onItemClick", function() {
+    $$("add_window").show();
+  });
+
+  // Обработка нажатия на кнопку закрытия формы добавления нового товара
+  $$("close_form_button").attachEvent("onItemClick", function() {
+    $$("add_form").clear();
+    $$("add_form").clearValidation();
+    $$("add_window").hide();
+  });
+
+  // Обработка нажатия на кнопу "Добавить" в окне добавления нового товара
+  $$("add_product").attachEvent("onItemClick", function() {
+    if ($$("add_form").validate()) {
+      addToStock($$("add_form").getValues());
+      refresh();
+      $$("add_form").clear();
+      $$("add_window").hide();
+    }
+  });
+
+  // Функция перемещения товаров из одной таблицы в другую
   function transfer(fromStorage, toStorage, product) {
     if (fromStorage.exists(product.id)) {
       fromStorage.getItem(product.id).quantity--;
@@ -152,31 +163,40 @@ webix.ready(function () {
     refresh();
   }
 
+  // Функция проверки наличия подобного товара в таблице
   function addTo(toStorage, item) {
+    // если подобный товар есть в таблице, увеличивается его количество
     if (toStorage.exists(item.id)) {
       toStorage.getItem(item.id).quantity++;
-    } else {
+    }
+    // если подобного товара нет в таблице, добавляется новая строка с этим товаром
+    else {
       toStorage.add({ id: item.id, title: item.title, quantity: 1, cost: item.cost });
     }
+    // подсчет суммы товаров в корзине
     $$("summary").getItem("sum").value = 0;
     $$("basket").data.each(function (item) {
       $$("summary").getItem("sum").value += item.cost * item.quantity;
     });
   }
 
+  // Функция добавления нового товара из формы в таблицу "Склад"
   function addToStock(item) {
+    // если в таблице есть товар с такими же именем и ценой, как у добавляемого,
+    // количество товара в таблице увеличивается на количество, переданное из формы
     for (let i = 1; i <= $$("stock").count(); i++) {
-      console.log($$("stock").getItem(i).cost)
       if ($$("stock").getItem(i).title.toLowerCase() === item.title.toLowerCase() &&
           $$("stock").getItem(i).cost === Number(item.cost)) {
         $$("stock").getItem(i).quantity += Number(item.quantity);
         return;
       };
     }
+    // если подобного товара нет в таблице, добавляется новая строка с данными из формы
     $$("stock").add({ id: $$("stock").count() + 1, title: item.title,
         quantity: Number(item.quantity), cost: Number(item.cost) });
   }
 
+  // Функция обновления данных в таблицах и строки с суммой
   function refresh() {
     $$("stock").refresh();
     $$("basket").refresh();
